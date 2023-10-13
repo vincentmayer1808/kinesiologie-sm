57b8d2acd7cfc4d31006196a4b5406970bec69b4
vincent <vincent.mayer.1808@gmail.com>
2023-10-13 15:24:33 -0300

    computer change

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Kinesiologie SM',
-  description: 'Kinesiologie Sabrina Mayer',
+  description: 'Site Internet de Kinesiologie Sabrina Mayer',
 }
 
 export default function RootLayout({ children }) {
